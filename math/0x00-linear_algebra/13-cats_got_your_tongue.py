a0f9f107e523d4045edc1a2587659ab2134ff775
Justin Masayda <justin.masayda@gmail.com>
2021-12-01 15:30:46 -0500

    import numpy as np

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Defines `np_cat`."""
-import numpy
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
     """Concatenates two matrices along a specific axis."""
-    return numpy.concatenate((mat1, mat2), axis)
+    return np.concatenate((mat1, mat2), axis)
