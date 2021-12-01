647435fedfc570b2583bc283c390ea56ced522f2
Justin Masayda <justin.masayda@gmail.com>
2021-12-01 12:22:00 -0500

    Remove illegal import

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """Defines `add_arrays`."""
-from itertools import zip_longest
 
 
 def add_arrays(arr1, arr2):
     """Adds two arrays element-wise."""
-    try:
-        return [sum(tuple) for tuple in zip_longest(arr1, arr2)]
-    except TypeError:
+    if len(arr1) != len(arr2):
         return None
+    return [sum(tuple) for tuple in zip(arr1, arr2)]
