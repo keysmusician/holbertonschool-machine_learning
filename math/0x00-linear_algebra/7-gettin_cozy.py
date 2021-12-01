d3119a14bf6dd3435db250b6b8d6f9d438267d02
Justin Masayda <justin.masayda@gmail.com>
2021-12-01 12:42:12 -0500

    Remove test

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -10,22 +10,3 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return mat1 + mat2
     elif axis == 1 and len(mat1) == len(mat2):
         return [row + mat2[index] for index, row in enumerate(mat1)]
-
-
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-print(mat4)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat5)
-
-# Mutate mat1:
-mat1[0] = [9, 10]
-mat1[1].append(5)
-print(mat1)
-
-# Ensure mutation does not affect return value of cat_matrices2D:
-print(mat4)
-print(mat5)
