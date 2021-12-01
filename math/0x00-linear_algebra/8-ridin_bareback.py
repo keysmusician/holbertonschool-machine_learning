93362c56b735246ed9425f886c1ec44021191f97
Justin Masayda <justin.masayda@gmail.com>
2021-12-01 13:27:41 -0500

    Fix formatting

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -5,16 +5,16 @@
 def mat_mul(mat1, mat2):
     """Performs 2D-matrix multiplication."""
     if len(mat1) == 0 or len(mat1[0]) != len(mat2):
-      return None
+        return None
     # transpose matrix 2
     mat2 = list(zip(*mat2))
     dot_products = list()
     for mat1_row in mat1:
-      dot_products.append([])
-      for mat2_row in mat2:
-        # Compute dot product
-        products = [a * b for a, b in zip(mat1_row, mat2_row)]
-        dot_product = sum(products)
-        dot_products[-1].append(dot_product)
+        dot_products.append([])
+        for mat2_row in mat2:
+            # Compute dot product
+            products = [a * b for a, b in zip(mat1_row, mat2_row)]
+            dot_product = sum(products)
+            dot_products[-1].append(dot_product)
 
     return dot_products
