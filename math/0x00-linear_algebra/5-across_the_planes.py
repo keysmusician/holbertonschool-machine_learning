eaef95dda17f467161c09d10b0f30c6673381a7d
Justin Masayda <justin.masayda@gmail.com>
2021-12-01 12:40:49 -0500

    Rewrite algorithm

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -5,8 +5,17 @@ from itertools import zip_longest
 
 def add_matrices2D(mat1, mat2):
     """Adds two matrices element-wise."""
-    try:
-        return [sum(tuple) for rows in zip_longest(mat1, mat2)
-                for tuple in zip_longest(*rows)]
-    except TypeError:
+    if len(mat1) != len(mat2):
         return None
+    if len(mat1) == 0:
+        return []
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    matrix = list()
+    for row, row_pair in enumerate(zip(mat1, mat2)):
+        matrix.append([])
+        for tuple in zip(row_pair[0], row_pair[1]):
+            matrix[row].append(sum(tuple))
+
+    return matrix
