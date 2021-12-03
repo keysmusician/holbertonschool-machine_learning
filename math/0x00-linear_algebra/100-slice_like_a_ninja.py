5d95d14c53f352fdb845510f3b19a4ed40392bb3
Justin Masayda <justin.masayda@gmail.com>
2021-12-03 13:12:13 -0500

    Remove test code

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Defines `np_slice`."""
-import numpy as np
 
 
 def np_slice(matrix, axes={}):
@@ -15,11 +14,3 @@ def np_slice(matrix, axes={}):
             slices.append(slice(None, None, None))
 
     return matrix[tuple(slices)]
-
-
-mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
-                 [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
-print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
-print()
-print(mat2)
