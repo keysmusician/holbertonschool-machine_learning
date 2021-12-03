669fd41196635d0db5801947a0c096923c8362cc
Justin Masayda <justin.masayda@gmail.com>
2021-12-03 13:11:34 -0500

    Correct task 15

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 """Defines `np_slice`."""
+import numpy as np
 
 
 def np_slice(matrix, axes={}):
     """Slices a matrix along specific axes."""
-    sorted_axes_items = list(axes.items())
-    sorted_axes_items.sort
-    max_index = sorted_axes_items[-1][0]
-    index = [Ellipsis for _ in range(max_index + 1)]
-    for item in sorted_axes_items:
-        index[item[0]] = slice(*item[1])
-    return matrix[tuple(index)]
+    slices = []
+
+    for i in range(matrix.ndim):
+        t = axes.get(i)
+        if t is not None:
+            slices.append(slice(*t))
+        else:
+            slices.append(slice(None, None, None))
+
+    return matrix[tuple(slices)]
+
+
+mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+                 [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
+print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
+print()
+print(mat2)
