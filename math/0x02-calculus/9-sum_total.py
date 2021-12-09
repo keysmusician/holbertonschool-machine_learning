c35f672c3d238c7df2070342fb9b12860c80f1bb
Justin Masayda <justin.masayda@gmail.com>
2021-12-09 16:17:07 -0500

    Compliant with "no loops" restriction

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,4 +4,4 @@
 
 def summation_i_squared(n):
     """Calculates the sum of squares of numbers from 1 to n."""
-    return sum([m ** 2 for m in range(1, n + 1)])
+    return sum(map(lambda i: i ** 2, range(1, n + 1)))
