773379068834ebbca386a96b858b82660bcf2a49
Justin Masayda <justin.masayda@gmail.com>
2021-12-09 17:01:09 -0500

    Pycodestyle

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -7,8 +7,8 @@ def poly_derivative(poly):
     Calculates the derivative of a polynomial from a list of its coefficients.
     """
     if not poly or \
-        type(poly) is not list or \
-        not all([type(n) in (int, float) for n in poly]):
+            type(poly) is not list or \
+            not all([type(n) in (int, float) for n in poly]):
         return None
 
     derivative = [i * n for i, n in enumerate(poly)][1:]
