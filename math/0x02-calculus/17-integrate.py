0672cfcac3a3b30b04a2632c7819b5036ea5c9ae
Justin Masayda <justin.masayda@gmail.com>
2021-12-09 17:58:13 -0500

    Truncate trailing zeros

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -13,9 +13,18 @@ def poly_integral(poly, C=0):
             not all([type(n) in (int, float) for n in poly]):
         return None
 
+    # Truncate trailing zeros:
+    try:
+        while poly[-1] == 0:
+            del poly[-1]
+    except IndexError:
+        pass
+
     poly = [n / (i + 1) for i, n in enumerate(poly)]
     poly = [C] + poly
+
     # Convert all whole number floats to integers
     poly = [int(n) if type(n) is float and n.is_integer() else n
             for n in poly]
+
     return poly
