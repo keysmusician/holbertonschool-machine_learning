f449c53de3e27e7d7b3a41bf55bed9a63c7a0a44
Justin Masayda <justin.masayda@gmail.com>
2021-12-09 17:47:46 -0500

    Use MATH

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,4 +5,4 @@
 def summation_i_squared(n):
     """Calculates the sum of squares of numbers from 1 to `n`."""
     if type(n) is int and n > 0:
-        return sum(map(lambda i: i ** 2, range(1, n + 1)))
+        return (n * (n + 1) * (2 * n + 1)) / 6
