76394f5895b92b876d55f194ad74e749b0a5020c
Justin Masayda <justin.masayda@gmail.com>
2021-12-15 14:47:04 -0500

    Add cdf & erf

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -27,6 +27,7 @@ class Normal:
             self.stddev = variance ** .5
 
     def z_score(self, x):
+        """Calculates the z-score of `x`."""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
@@ -37,3 +38,22 @@ class Normal:
         """The probability density function."""
         exponent = self.z_score(x) ** 2 / 2
         return 1 / (self.stddev * self.tau ** .5 * self.e ** exponent)
+
+    def erf(self, x):
+        """The error function."""
+        return (
+            2 / self.pi ** .5
+            * (
+                + x
+                - x ** 3 / 3
+                + x ** 5 / 10
+                - x ** 7 / 42
+                + x ** 9 / 216
+            )
+        )
+
+    def cdf(self, x):
+        """The cumulative distribution function."""
+        error_x = (x - self.mean) / (self.stddev * 2 ** .5)
+        return (1 + self.erf(error_x)) / 2
+
