46841237a6640a83321f8bfcbfed29dfc4b0cbae
Justin Masayda <justin.masayda@gmail.com>
2021-12-15 01:27:59 -0500

    Add CDF

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -26,3 +26,10 @@ class Exponential:
             return 0
 
         return self.lambtha * self.e ** (-self.lambtha * x)
+
+    def cdf(self, x):
+        """The cumulative distribution function."""
+        if x < 0:
+            return 0
+
+        return 1 - self.e ** (-self.lambtha * x)
