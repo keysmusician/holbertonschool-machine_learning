6c08e62ccd92c192cc322f78d74937d9f19d6fb3
Justin Masayda <justin.masayda@gmail.com>
2021-12-15 00:32:42 -0500

    Add Exponential class

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,18 @@
+"""Defines `Exponential`."""
+
+
+class Exponential:
+    """An exponential distribution."""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes an exponential distribution."""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError('lambtha must be a positive value')
+            self.lambtha = float(lambtha)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            self.lambtha = sum(data) / len(data)
