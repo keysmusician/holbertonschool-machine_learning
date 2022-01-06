b6f2c6b05ac4905630ab6a14416ea7e6ecd5fa79
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 03:07:51 -0500

    Add task 1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""Defines `create_placeholders`."""
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Creates a neural network layer.
+
+    Args:
+        prev: The tensor output of the previous layer.
+        n: The number of neurons desired in the layer.
+        activation: The activation function that the layer should use.
+
+    Returns: The tensor output of the layer.
+    """
+    layer = tf.layers.Dense(n, activation, name="layer")
+    return layer(prev)
