b31129aaefeb469dba9c5154eb5538a74797e6b4
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 03:47:56 -0500

    Add task 2

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""Defines `forward_prop`."""
+import tensorflow.compat.v1 as tf
+
+
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    Creates the forward propagation graph for the neural network.
+
+    Args:
+        x: A placeholder for the input data.
+        layer_sizes: A list containing the number of nodes in each layer of the
+            network
+        activations: A list containing the activation functions for each layer
+            of the network
+
+    Returns: A tensor for the prediction of the network.
+    """
+    input = x
+    for layer_size, activation_function in zip(layer_sizes, activations):
+        input = create_layer(input, layer_size, activation_function)
+
+    return input
