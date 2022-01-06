79e2f20fc0881d62bd326bcaa0350f90cb9a634d
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 03:15:27 -0500

    Use He Normal initialization

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -7,6 +7,8 @@ def create_layer(prev, n, activation):
     """
     Creates a neural network layer.
 
+    Initializes weights with He Normal initialization.
+
     Args:
         prev: The tensor output of the previous layer.
         n: The number of neurons desired in the layer.
@@ -14,5 +16,11 @@ def create_layer(prev, n, activation):
 
     Returns: The tensor output of the layer.
     """
-    layer = tf.layers.Dense(n, activation, name="layer")
+    layer = tf.layers.Dense(
+        activation=activation,
+        name="layer",
+        units=n,
+        kernel_initializer=tf.keras.initializers.
+            VarianceScaling(mode='fan_avg')
+    )
     return layer(prev)
