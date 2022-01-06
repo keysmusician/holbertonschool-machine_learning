1f9e92e4c0e5e407f903da40d3f4096caf15ad22
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 15:26:46 -0500

    Add task 5

--- supervised_learning/0x02-tensorflow/5-create_train_op.py
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""Defines `create_train_op`."""
+import tensorflow.compat.v1 as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Creates the training operation for the network.
+
+    Args:
+        loss: The loss of the network's prediction.
+        alpha: The learning rate.
+
+    Returns: An operation that trains the network using gradient descent.
+    """
+    optimizer = tf.train.GradientDescentOptimizer(alpha)
+    train = optimizer.minimize(loss)
+    return train
