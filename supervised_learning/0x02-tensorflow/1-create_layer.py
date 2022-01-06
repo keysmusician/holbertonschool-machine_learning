b5ab58cfaba22b78aae4b95282c65c6351b5fd8f
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 03:20:35 -0500

    Add task 1

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Defines `create_layer`."""
+import tensorflow.compat.v1 as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Creates a neural network layer.
+
+    Initializes weights with He Normal initialization.
+
+    Args:
+        prev: The tensor output of the previous layer.
+        n: The number of neurons desired in the layer.
+        activation: The activation function that the layer should use.
+
+    Returns: The tensor output of the layer.
+    """
+    layer = tf.layers.Dense(
+        activation=activation,
+        name="layer",
+        units=n,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            mode='fan_avg')
+    )
+    return layer(prev)
