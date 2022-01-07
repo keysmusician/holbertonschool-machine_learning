79dbb0624baf49a1bf8cf185c1ff380b9136973e
Justin Masayda <justin.masayda@gmail.com>
2022-01-06 21:10:56 -0500

    Testing Kevin's solution

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -14,10 +14,18 @@ def evaluate(X, Y, save_path):
 
     Returns: the network's prediction, accuracy, and loss, respectively
     """
+    with tf.Session() as sess:
+        loader = tf.train.import_meta_graph(save_path + ".meta")
+        loader.restore(sess, save_path)
 
-    with tf.Session() as session:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
-        saver.restore(session, save_path)
-        x, y, y_pred, loss, accuracy, train = tf.get_collection("model")
-        routine = (y_pred, accuracy, loss)
-        return session.run(routine, {x: X, y: Y})
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        eval_prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        eval_accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
+        eval_loss = sess.run(loss, feed_dict={x: X, y: Y})
+
+    return eval_prediction, eval_accuracy, eval_loss
