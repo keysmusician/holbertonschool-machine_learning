fd339f1f684f02c906192e6630be1e20e0849a6d
Justin Masayda <justin.masayda@gmail.com>
2022-01-09 18:56:03 -0500

    Add task 25

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines `one_hot_decode`."""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    Converts a one-hot matrix into a numeric label vector.
+
+    Args:
+        one_hot: A one-hot matrix.
+
+    Returns: A decoding of `one_hot` with shape (classes, traing examples), or
+        None on failure.
+    """
+    if one_hot.ndim == 2:
+        return np.argmax(one_hot, axis=0)
+
