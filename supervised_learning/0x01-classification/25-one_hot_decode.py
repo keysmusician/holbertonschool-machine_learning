007357955d568fb3c141854b3ab6f298ba6129ea
Justin Masayda <justin.masayda@gmail.com>
2022-01-09 18:54:39 -0500

    Update documentation & validation

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Defines `one_hot_encode`."""
+"""Defines `one_hot_decode`."""
 import numpy as np
 
 
@@ -8,13 +8,11 @@ def one_hot_decode(one_hot):
     Converts a one-hot matrix into a numeric label vector.
 
     Args:
-        Y: The correct labels of the training data.
-        classes: The number of classes of the training data.
+        one_hot: A one-hot matrix.
 
-    Returns: A one-hot deencoding of Y with shape (classes, m), or None on
-        failure.
+    Returns: A decoding of `one_hot` with shape (classes, traing examples), or
+        None on failure.
     """
-    try:
+    if one_hot.ndim == 2:
         return np.argmax(one_hot, axis=0)
-    except Exception:
-        return None
+
