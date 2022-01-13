56820d772944bddd035735c64fb36c8d163879f5
Justin Masayda <justin.masayda@gmail.com>
2022-01-13 03:04:10 -0500

    Add task 7

---
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Defines `update_variables_RMSProp`."""
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    Updates a variable using the RMSProp optimization algorithm.
+
+    alpha: The learning rate.
+    beta2: The RMSProp weight.
+    epsilon: A small number to avoid division by zero.
+    var: A numpy.ndarray containing the variable to be updated.
+    grad: A numpy.ndarray containing the gradient of var.
+    s: The previous second moment of var.
+
+    Returns: The updated variable and the new moment, respectively.
+    """
+    s = beta2 * s + (1 - beta2) * grad ** 2
+    var -= alpha * grad / (s ** (1/2) + epsilon)
+    return (var, s)
