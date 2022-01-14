daf5409aaf36c58b0594a5482cc624c0c783a706
Justin Masayda <justin.masayda@gmail.com>
2022-01-13 19:55:45 -0500

    Remove import

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 """Trains a neural network with optimizations."""
 import tensorflow.compat.v1 as tf
-shuffle_data = __import__('2-shuffle_data').shuffle_data
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """
+    Shuffles the data points in two matrices the same way.
+
+    Args:
+        X: A numpy.ndarray of shape (m, nx) to shuffle, where 'm' is the number
+            of data points and 'nx' is the number of features in X.
+        Y: A numpy.ndarray of shape (m, ny) to shuffle, where 'm' is the same
+            number of data points as in X and 'ny' is the number of features in
+            Y.
+
+    Returns: The shuffled X and Y matrices.
+    """
+    if (X.shape[0] == Y.shape[0]):
+        random_order = np.random.permutation(X.shape[0])
+        return X[random_order], Y[random_order]
 
 
 def calculate_accuracy(y, y_pred):
