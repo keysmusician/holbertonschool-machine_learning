addc654687f8c9e000e73c377043b58a82a4f8a5
Justin Masayda <justin.masayda@gmail.com>
2022-01-17 18:24:11 -0500

    pycodestyle

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -4,6 +4,7 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(confusion):
     """
     Calculates the F1 score of a confusion matrix.
