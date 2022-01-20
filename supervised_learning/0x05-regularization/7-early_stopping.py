532ce62e8f2abcbfe9fd0e327aa341d4fcbd70f2
Justin Masayda <justin.masayda@gmail.com>
2022-01-20 17:38:57 -0500

    Add task 7

---
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Defines `early_stopping`."""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """
+    Determines if gradient descent should halt early.
+
+    Early stopping occurs when the validation cost of the network has not
+    decreased relative to the optimal validation cost by more than the
+    threshold over a specific patience count.
+
+    cost: The current validation cost of the neural network.
+    opt_cost: The lowest recorded validation cost of the neural network.
+    threshold: The threshold used for early stopping.
+    patience: The patience count used for early stopping.
+    count: The count of how long the threshold has not been met.
+
+    Returns: A boolean of whether the network should be stopped early, followed
+        by the updated count.
+    """
+    if count > patience:
+        return (True, count)
+    elif opt_cost - cost > threshold:
+        return (False, 0)
+    else:
+        return (False, count + 1)
