1996fbad9407c034401849245e8c95da2c9ff0d4
Justin Masayda <justin.masayda@gmail.com>
2022-01-20 17:14:09 -0500

    Add task 4

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Defines `dropout_forward_prop`."""
+import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
@@ -19,3 +20,29 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     Returns: A dictionary containing the outputs of each layer and the dropout
         mask used on each layer.
     """
+    # Cache of activations and dropout masks:
+    cache = {'A0': X}
+    # Previous layer activation:
+    A = X
+    for layer in range(1, L + 1):
+        # Weights:
+        W = weights["W{}".format(layer)]
+        # Bias:
+        b = weights["b{}".format(layer)]
+        # Linear transformation:
+        Z = W @ A + b
+        if layer < L:
+            # Activation:
+            A = np.tanh(Z)
+            # Dropout mask:
+            D = np.random.rand(*A.shape) < keep_prob
+            D = np.where(D, 1, 0)
+            cache['D{}'.format(layer)] = D
+            # Apply dropout mask:
+            A *= D / keep_prob
+        else:
+            # No dropout on final layer; softmax activation:
+            A = np.exp(Z) / np.sum(np.exp(Z), axis=0)
+        cache['A{}'.format(layer)] = A
+
+    return cache
