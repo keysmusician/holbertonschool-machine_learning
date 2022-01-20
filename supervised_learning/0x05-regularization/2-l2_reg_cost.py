87d6015961131f4b0aa6743ba0b1b26e8d4fd1e4
Justin Masayda <justin.masayda@gmail.com>
2022-01-20 06:24:21 -0500

    Add task 2

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Defines `l2_reg_cost`."""
+import tensorflow.compat.v1 as tf
 
 
 def l2_reg_cost(cost):
@@ -12,3 +13,4 @@ def l2_reg_cost(cost):
     Returns: A tensor containing the cost of the network accounting for L2
         regularization.
     """
+    return cost + tf.losses.get_regularization_losses()
