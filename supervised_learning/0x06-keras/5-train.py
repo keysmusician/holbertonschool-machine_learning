6e52b35a4d40e6a52bcf3d6bbd1248eefbade8a0
Justin Masayda <justin.masayda@gmail.com>
2022-01-25 17:43:39 -0500

    Add task 5

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Defines `train_model`."""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent.
+
+    network: The model to train.
+    data: A numpy.ndarray of shape (m, nx) containing the input data.
+    labels: A one-hot numpy.ndarray of shape (m, classes) containing the labels
+        of data.
+    batch_size: The size of the batch used for mini-batch gradient descent.
+    epochs: The number of passes through data for mini-batch gradient descent.
+    validation_data: The data to validate the model with, if not None.
+    verbose: A boolean that determines if output should be printed during
+        training.
+    shuffle: A boolean that determines whether to shuffle the batches every
+        epoch.
+
+    Returns: The History object generated after training the model.
+    """
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        shuffle=shuffle,
+        verbose=verbose,
+        validation_data=validation_data
+    )
