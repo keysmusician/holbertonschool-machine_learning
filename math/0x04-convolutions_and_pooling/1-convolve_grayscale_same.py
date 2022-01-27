769f9d7cc893e4ad01cf37d286a8da79964cf5f4
Justin Masayda <justin.masayda@gmail.com>
2022-01-26 22:36:07 -0500

    Add task 1

---
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Defines `convolve_grayscale_same`"""
+from math import ceil, floor
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """
+    Performs a same convolution on grayscale images.
+
+    images: A numpy.ndarray with shape (m, h, w) containing multiple grayscale
+        images.
+        - m is the number of images.
+        - h is the height in pixels of the images.
+        - w is the width in pixels of the images.
+    kernel: A numpy.ndarray with shape (kh, kw) containing the kernel for the
+        convolution.
+        - kh is the height of the kernel.
+        - kw is the width of the kernel.
+
+    Returns: A numpy.ndarray containing the convolved images.
+    """
+    kernel_width, kernel_height = kernel.shape
+    image_count, image_width, image_height = images.shape
+
+    pad_width = (kernel_width - 1) // 2
+    pad_height = (kernel_height - 1) // 2
+    padding = (
+        [0, 0],
+        [floor(pad_width / 2), ceil(pad_width / 2)],
+        [floor(pad_height / 2), ceil(pad_height / 2)]
+    )
+    padded_images = np.pad(images, padding)
+
+    output_shape = (
+        image_count,
+        image_width + pad_width - kernel_height + 1,
+        image_height + pad_height - kernel_width + 1
+    )
+    output = np.zeros(output_shape)
+    for top in range(output_shape[1]):
+        for left in range(output_shape[2]):
+            view = padded_images[
+                :,
+                top:top + kernel_height,
+                left:left + kernel_width
+            ]
+            output[:, top, left] = np.sum(view * kernel, axis=(1, 2))
+
+    return output
