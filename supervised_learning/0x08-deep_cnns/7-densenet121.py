0a5fb37ab279cbec34343b54ccb73f222bde8a0d
Justin Masayda <justin.masayda@gmail.com>
2022-02-09 15:43:52 -0500

    Fix docstring

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Defines `transition_layer` """
+""" Defines `densenet121` """
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
