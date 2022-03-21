22f1a21229656d389cfe67ab0640e0fc2c5d39e2
Justin Masayda <justin.masayda@gmail.com>
2022-03-21 19:56:59 -0400

    Add task 1

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Defines `mean_cov`."""
+import numpy as np
+
+
+def correlation(C):
+    """
+    Calculates a correlation matrix.
+
+    C: A numpy.ndarray of shape (d, d) containing a covariance matrix:
+        d: The number of dimensions
+
+    Returns: A numpy.ndarray of shape (d, d) containing the correlation matrix.
+    """
+    if not type(C) is np.ndarray:
+        raise TypeError('C must be a numpy.ndarray')
+    elif len(C.shape) != 2:
+        raise ValueError('C must be a 2D square matrix')
+
+    root_variances =np.sqrt(np.diag(C))
+
+    return C / np.outer(root_variances, root_variances)
