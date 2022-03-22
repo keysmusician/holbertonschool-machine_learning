77c3767da719637d5b2d305bc447f396e8b0f4a0
Justin Masayda <justin.masayda@gmail.com>
2022-03-21 23:04:32 -0400

    Add task 3

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -24,6 +24,31 @@ class MultiNormal:
         mu = self.mean
         self.cov = np.matmul(data - mu, data.T - mu.T) / (sample_count - 1)
 
+    def pdf(self, x):
+        """
+        Calculates the probability density at a data point.
 
-        def pdf(self, x):
-            """"""
+        x: A numpy.ndarray of shape (d, 1) containing the data point whose PDF
+            should be calculated:
+            d: The number of dimensions of the MultiNormal instance
+
+        Returns: The value of the PDF at a point.
+        """
+        if type(x) != np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        d0, d1 = x.shape
+        if d0 != d or d1 != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        tau = np.pi * 2
+        probability_density = np.exp(
+                -1/2 *
+                (x - self.mean).T @
+                np.linalg.inv(self.cov) @
+                (x - self.mean)
+            ) / np.sqrt(tau ** d * np.linalg.det(self.cov))
+
+        return np.asscalar(probability_density)
