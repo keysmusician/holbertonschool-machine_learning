1cfea17c747fffbf8ed4715aaeb77f95bd8ce2a9
Justin Masayda <justin.masayda@gmail.com>
2022-03-24 21:49:21 -0400

    Update input validation

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -23,24 +23,27 @@ def intersection(x, n, P, Pr):
     Returns: A 1D numpy.ndarray containing the likelihood of obtaining the
         data, `x` and `n`, for each probability in `P`, respectively.
     """
-    if type(n) is not int or n < 1:
-        raise ValueError('n must be a positive integer')
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
-        raise ValueError(
-            'x must be an integer that is greater than or equal to 0')
+        error = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(error)
     if x > n:
-        raise ValueError('x cannot be greater than n')
-    if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise TypeError('P must be a 1D numpy.ndarray')
-    if type(P) is not np.ndarray or Pr.shape != P.shape:
-        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
-    for p, pr in zip(P, Pr):
-        if p < 0 or p > 1:
-            raise ValueError('All values in P must be in the range [0, 1]')
-        if pr < 0 or pr > 1:
-            raise ValueError('All values in Pr must be in the range [0, 1]')
-    if not np.isclose(sum(Pr), 1):
-        raise ValueError('Pr must sum to 1')
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray):
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    for num in P:
+        if num < 0 or num > 1:
+            raise ValueError("All values in P must be in the range [0, 1]")
+    for num in Pr:
+        if num < 0 or num > 1:
+            raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
 
     factorial = np.math.factorial
     n_choose_x = factorial(n) / (factorial(x) * factorial(n - x))
