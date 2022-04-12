05a6c587432e94008f867d88c4049bd5b5e2a538
Justin Masayda <justin.masayda@gmail.com>
2022-04-12 19:07:15 -0400

    Add task 0

---
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Defines `GaussianProcess`."""
+import numpy as np
+
+
+class GaussianProcess:
+    """A noiseless 1D Gaussian process."""
+
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        X_init: A numpy.ndarray of shape (t, 1) representing the inputs already
+            sampled with the black-box function.
+        Y_init: A numpy.ndarray of shape (t, 1) representing the outputs of the
+            black-box function for each input in X_init.
+        t: The number of initial samples.
+        l: The length parameter for the kernel.
+        sigma_f: The standard deviation given to the output of the black-box
+            function.
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(X_init, X_init)
+
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix between two matrices using a
+        Gaussian/squared exponential/Radial Basis Function (RBF) kernel.
+
+        X1: A numpy.ndarray of shape (m, 1).
+        X2: A numpy.ndarray of shape (n, 1).
+
+        Returns: the covariance kernel matrix as a numpy.ndarray of shape
+            (m, n).
+        """
+        sqdist = (
+            np.sum(X1**2, 1).reshape(-1, 1) +
+            np.sum(X2**2, 1) -
+            2 * np.dot(X1, X2.T)
+        )
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
