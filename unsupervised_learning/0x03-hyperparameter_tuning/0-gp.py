cd8cce8173e3660772425d48430e0c665dbb3db3
Justin Masayda <justin.masayda@gmail.com>
2022-04-14 12:59:07 -0400

    Pycodestyle

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -37,8 +37,8 @@ class GaussianProcess:
             (m, n).
         """
         sqdist = (
-            np.sum(X1**2, 1).reshape(-1, 1) +
-            np.sum(X2**2, 1) -
+            np.sum(X1 ** 2, 1).reshape(-1, 1) +
+            np.sum(X2 ** 2, 1) -
             2 * np.dot(X1, X2.T)
         )
-        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
