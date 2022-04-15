f56e171644123d8763c4c99fbbe1b1e3bb5bfb0c
Justin Masayda <justin.masayda@gmail.com>
2022-04-14 23:14:09 -0400

    Fix pycodestyle

--- unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/0-gp.py
@@ -36,9 +36,8 @@ class GaussianProcess:
         Returns: the covariance kernel matrix as a numpy.ndarray of shape
             (m, n).
         """
-        sqdist = (
-            np.sum(X1 ** 2, 1).reshape(-1, 1) +
-            np.sum(X2 ** 2, 1) -
-            2 * np.dot(X1, X2.T)
-        )
+        x1 = np.sum(X1 ** 2, 1).reshape(-1, 1)
+        x2 = np.sum(X2 ** 2, 1)
+        sqdist = x1 + x2 - 2 * X1 @ X2.T
+
         return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
