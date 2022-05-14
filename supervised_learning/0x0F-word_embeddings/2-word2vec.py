8de70ae47783fdcc7a532601861aac10e53952f0
Justin Masayda <justin.masayda@gmail.com>
2022-05-13 20:19:27 -0400

    Pycodestyle

--- supervised_learning/0x0F-word_embeddings/2-word2vec.py
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -4,10 +4,10 @@ from gensim.models import Word2Vec
 
 
 def word2vec_model(
-    sentences, size=100, min_count=5, window=5, negative=5, cbow=True,
-    iterations=5, seed=0, workers=1):
+        sentences, size=100, min_count=5, window=5, negative=5, cbow=True,
+        iterations=5, seed=0, workers=1):
     """
-    Creates and trains a gensim word2vec model.
+    Creates and trains a Gensim word2vec model.
 
     sentences: A list of sentences to be trained on.
     size: The dimensionality of the embedding layer.
@@ -33,6 +33,5 @@ def word2vec_model(
         seed=seed,
         workers=workers,
         sg=(not cbow)
-        )
-
+    )
     return model
