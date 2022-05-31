d025083bbe13fc5c35bd8f6028aa4d479ba6925e
Justin Masayda <justin.masayda@gmail.com>
2022-05-31 18:15:32 -0400

    Correction of self-attention calculation

--- supervised_learning/0x11-attention/1-self_attention.py
+++ supervised_learning/0x11-attention/1-self_attention.py
@@ -27,13 +27,37 @@ class SelfAttention(tf.keras.layers.Layer):
 
         s_prev: A tensor of shape (batch, units) containing the previous
             decoder hidden state.
-        hidden_states: A tensor of shape (batch, input_seq_len, units)
-            containing the outputs of the encoder.
+        hidden_states: Also called annotations, a tensor of shape (batch,
+            input_seq_len, units) containing the outputs of the encoder.
 
-        Returns: (context, weights)
-            context: A tensor of shape (batch, units) that contains the context
-                vector for the decoder.
+        Returns: (context_vector, weights)
+            context_vector: A tensor of shape (batch, units) that contains the
+                context vector for the decoder.
             weights: A tensor of shape (batch, input_seq_len, 1) that contains
-                the attention weights.
+                the attention/alignment model weights.
         """
-        return self.V(tf.tanh(self.W(s_prev) + self.U(hidden_states)))
+        # hidden states/annotations shape = (batch_size/sentence_count,
+        # word/token_count, unit_count/latent_features)
+
+        # alignment scores & weights shape =
+        # (batch_size/sentence_count, 1 [expanded dimension], word/token_count)
+
+        # The alignment model:
+        alignment_scores = self.V(
+            tf.nn.tanh(self.W(s_prev)[:, tf.newaxis] + self.U(hidden_states)))
+
+        alignment_weights = tf.nn.softmax(alignment_scores, axis=1)
+
+        # Calculate the context vector (aka the expected annotation).
+        # Swap alignment_weights axis 1 (added for broadcasting agreement) with
+        # axis 2 (words/tokens), then matrix multiply. Finally, remove the
+        # expanded dimension that was added for broadcasting:
+        # context_vector = (
+        #   tf.transpose(alignment_weights, [0, 2, 1]) @ hidden_states
+        # )[:, 0]
+
+        # Simpler calculation:
+        context_vector = tf.reduce_sum(
+            alignment_weights * hidden_states, axis=1)
+
+        return context_vector, alignment_weights
