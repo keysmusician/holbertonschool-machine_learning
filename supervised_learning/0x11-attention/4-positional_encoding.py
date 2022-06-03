347e3abafa9443ab73ad869690713a6b01067bbd
Justin Masayda <justin.masayda@gmail.com>
2022-06-03 00:25:26 -0400

    Complete task 4

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -4,5 +4,15 @@ import numpy as np
 
 
 def positional_encoding(max_seq_len, dm):
-    """ Calculates the positional encoding for a transformer. """
-    return np.zeros((max_seq_len, dm))
+    """
+    Calculates the sinusoidal positional encoding for a transformer.
+
+    max_seq_len: An integer representing the maximum sequence length.
+    dm: The model depth.
+
+    Returns: A `numpy.ndarray` of shape (max_seq_len, dm) containing the
+        positional encoding vectors.
+    """
+    yy, xx = np.meshgrid(np.arange(dm), np.arange(max_seq_len))
+
+    return np.sin(xx / 10000 ** (yy // 2 * 2 / dm) + np.pi / 2 * (yy % 2))
