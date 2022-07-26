933a54c2b8ce66c610dfc8555027639c025a7bec
Justin Masayda <justin.masayda@gmail.com>
2022-07-25 23:11:19 -0400

    Check for 'sentient' in 'classification'

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -15,18 +15,17 @@ def sentientPlanets():
     while (response.ok):
         response_JSON = response.json()
         for species in response_JSON.get('results', []):
-            if species.get('designation') == 'sentient':
+            if 'sentient' in (
+                species.get('designation'),
+                species['classification']
+            ):
                 try:
-                    if requests.get(species.get('homeworld')).json()['name'] == 'Rodia':
-                        print('Rodia')
                     sentient_host_planets.add(
                         requests.get(species.get('homeworld')).json()['name'])
                 except (
                     requests.exceptions.MissingSchema,
                     requests.JSONDecodeError
-                ) as error:
-                    print(species)
-                    print(error)
+                ):
                     continue
 
         next_page = response_JSON.get('next')
