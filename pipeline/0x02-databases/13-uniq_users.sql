a84eb00090b64cf819efd868d263f2ec7abcd7cd
Justin Masayda <justin.masayda@gmail.com>
2022-08-01 18:10:24 -0400

    Add task 13

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Creates a table of users
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
