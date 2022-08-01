b606e98919020777c58a5b5c4bc95b287b080af8
Justin Masayda <justin.masayda@gmail.com>
2022-08-01 00:36:33 -0400

    Reformat long queries

--- pipeline/0x02-databases/7-max_state.sql
+++ pipeline/0x02-databases/7-max_state.sql
@@ -1,2 +1,11 @@
 -- Displays the maximum temperature of each state ordered by state name
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC;
+SELECT
+    state, MAX(value)
+AS
+    max_temp
+FROM
+    temperatures
+GROUP BY
+    state
+ORDER BY
+    state ASC;
