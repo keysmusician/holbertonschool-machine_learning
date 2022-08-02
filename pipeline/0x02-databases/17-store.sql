a5e92a30a41f3faca04d9020647b4faef695666d
Justin Masayda <justin.masayda@gmail.com>
2022-08-01 20:37:57 -0400

    Add task 17

---
+++ pipeline/0x02-databases/17-store.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger that decreases the quantity of an item after adding a new
+-- order
+CREATE TRIGGER
+    decrease_quantity
+AFTER INSERT ON
+    orders
+FOR EACH ROW
+    UPDATE
+        items
+    SET
+        quantity = quantity - NEW.number
+    WHERE
+        items.name = NEW.item_name;
